8b41956759b1b8a53dcbd6efa33fcca168bb35d6
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-04-29 01:11:39 +0900

    とりあえずコピー

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 import discord
-from discord.ext import commands
-import sqlite3
+import re
+import datetime
 
-TOKEN="MTE2MjM2ODgyNTgyODg0MzU4MQ.GvwMX6.TeY9VmSdtJ2samVkSQcxHeWiLz21X9-WnrbiKM"
+TOKEN="MTE2MjM2ODgyNTgyODg0MzU4MQ.GERouF.uswQTOcwMgqZsdlhvnfdJzhsLTsd_w4FHl-zO0"
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.typing = False
+intents.reactions = True
+intents.guilds = True
 
 client = discord.Client(intents=intents)
 
-bot=commands.Bot(command_prefix="/",intents=intents)
-
-
+async def register_task(message:discord.Message):
+    this_year = datetime.date.today().year
+    date_str = re.findall(r"\d\d?/\d\d?\s\d\d?:\d\d", message.content)[0]
+    date = datetime.datetime.strptime(
+        f"{this_year}/"+date_str, "%Y/%m/%d %H:%M")
+    channel_id = message.channel.id
+    channel = message.channel
+    print(channel)
+    print(channel_id)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
     
-@bot.command()
-async def test(ctx,arg):
-    await ctx.send(arg)
+@client.event
+async def on_message(message:discord.Message):
+    pattern_task_register = r"【.*】\s*\[\d\d\/\d\d\s\d\d\:\d\d]"
+    is_task_register = re.fullmatch(pattern_task_register, message.content)
+    if is_task_register:
+        await register_task(message)
+    
+@client.event
+async def on_raw_message_edit(payload:discord.RawMessageUpdateEvent):
+    print(payload.data["content"])
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith(''):
-#         await message.channel.send('Hello!')
 
 client.run(token=TOKEN)
