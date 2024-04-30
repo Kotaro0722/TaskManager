14031a43ef845ca1b0ae91ad746e1aa46c7333dd
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-04-30 23:21:20 +0900

    不要部分を削除

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import re
 import datetime
 import config
 import pandas
-import asyncio
 from discord.ext import tasks
 from mydblib import my_update
 from mydblib2 import my_select
@@ -21,29 +20,6 @@ client = discord.Client(intents=intents)
 
 dbName=config.DBNAME
 task_table=config.TASK_TABLE
-
-async def register_task(message:discord.Message):
-    try:
-        thread_id = message.channel.id
-        message_id = message.id
-        
-        today=datetime.date.today()
-        year = today.year
-        date_str = re.findall(r"\d\d?/\d\d?\s\d\d?:\d\d", message.content)[0]
-        temp_date=datetime.datetime.strptime(date_str,"%m/%d %H:%M")
-        if today.month>temp_date.month:
-            year+=1
-        date_str = re.findall(r"\d\d?/\d\d?\s\d\d?:\d\d", message.content)[0]
-        date = datetime.datetime.strptime(
-            f"{year}/"+date_str, "%Y/%m/%d %H:%M")
-        
-        sql_insert_data=f"INSERT INTO {task_table}(message_id,thread_id,deadline) VALUES({message_id},{thread_id},'{date}')"
-        my_update(dbName,sql_insert_data)
-        
-        await message.add_reaction("⭕")
-        
-    except Exception as e:
-        print(e) 
     
 def select_tomorrow_task(list:pandas.DataFrame):
     today=datetime.datetime.today()
@@ -153,14 +129,35 @@ async def loop():
 async def on_ready():
     print(f'We have logged in as {client.user}')
     await loop.start()
-    
+
+async def register_task(message:discord.Message):
+    try:
+        thread_id = message.channel.id
+        message_id = message.id
+        
+        today=datetime.date.today()
+        year = today.year
+        date_str = re.findall(r"\d\d?/\d\d?\s\d\d?:\d\d", message.content)[0]
+        temp_date=datetime.datetime.strptime(date_str,"%m/%d %H:%M")
+        if today.month>temp_date.month:
+            year+=1
+        date_str = re.findall(r"\d\d?/\d\d?\s\d\d?:\d\d", message.content)[0]
+        date = datetime.datetime.strptime(
+            f"{year}/"+date_str, "%Y/%m/%d %H:%M")
+        
+        sql_insert_data=f"INSERT INTO {task_table}(message_id,thread_id,deadline) VALUES({message_id},{thread_id},'{date}')"
+        my_update(dbName,sql_insert_data)
+        
+        await message.add_reaction("⭕")
+        
+    except Exception as e:
+        print(e) 
     
 @client.event
 async def on_message(message:discord.Message):
     pattern_task_register = r"【.*】\s*\[\d\d?\/\d\d?\s\d\d?:\d\d?]"
     is_task_register = re.fullmatch(pattern_task_register, message.content)
     if is_task_register:
-        print("reg pass")
         await register_task(message)
     
 @client.event
